0fb0211a4c3e926697cf1d4fdf90067d3a15604f
Vishal18-sys <vy1043304@gmail.com>
2025-07-26 01:57:22 +0530

    Reverse an array of numbers and strings formate

---
+++ 3-ReverseArray-Program.js
@@ -0,0 +1,11 @@
+                //Reverse an array in string
+
+let arr = ["apple", 'orange', 'grapes','banana'];
+let reverse = [...arr].reverse();
+console.log(reverse);
+
+            // Same as Reverse an array in number
+    
+let arrNum = [1,2,3,4,5,6,7];
+let arrReverse = [...arrNum].reverse();
+console.log(arrReverse);
