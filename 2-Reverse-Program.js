2c256821ab972e2b51d28774242c20c4648d7f4a
Vishal18-sys <vy1043304@gmail.com>
2025-07-26 01:50:00 +0530

    Reverse a String or Number

---
+++ 2-Reverse-Program.js
@@ -0,0 +1,12 @@
+          // Reverse a string 
+
+let str = " hello buddy";
+let reverse = str.split('').reverse().join('');
+console.log(reverse);
+
+            //  Same as Reverse a Number
+
+let num = 1234567;
+let reverse1 = parseInt( num.toString().split('').reverse().join('') );
+console.log(reverse1);
+
