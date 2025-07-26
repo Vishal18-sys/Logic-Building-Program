2bff2aa13c9dc10c8aa2e5a5de3a0d7756e5e37d
Vishal18-sys <vy1043304@gmail.com>
2025-07-27 01:18:41 +0530

    Print a number b/w 1 to n and reverse a same no.

---
+++ 6-NumberProgram.js
@@ -0,0 +1,16 @@
+    // Print a number b/w 1 to n
+
+let num = 10;
+
+for(let i=1; i<=num; i++){
+    console.log(i);
+}
+
+console.log( )
+    // Print a reverse a number b/w n to 1
+
+let num1 = 10;
+
+for(let i=num1; i>=1; i--){
+    console.log(i);
+}
